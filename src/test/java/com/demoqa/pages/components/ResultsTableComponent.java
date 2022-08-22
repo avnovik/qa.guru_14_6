a89bb8842d9a159fcd9cae51641e20bb7cb6cb9d
Novikov Aleksei <3900676@gmail.com>
2022-08-22 22:59:28 +0300

    added SelenideElement

--- src/test/java/com/demoqa/pages/components/ResultsTableComponent.java
+++ src/test/java/com/demoqa/pages/components/ResultsTableComponent.java
@@ -1,5 +1,7 @@
 package com.demoqa.pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -7,22 +9,28 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class ResultsTableComponent {
 
+    SelenideElement
+            modalWindow = $(".modal-dialog"),
+            modalTitle = $("#example-modal-sizes-title-lg"),
+            resultsTable = $(".table-responsive table"),
+            closeModalWindowButton = $("#closeLargeModal");
+
     private final static String TITLE_TEXT = "Thanks for submitting the form";
 
     public void checkVisible() {
-        $(".modal-dialog").should(appear);
-        $("#example-modal-sizes-title-lg").shouldHave(text(TITLE_TEXT));
+        modalWindow.should(appear);
+        modalTitle.shouldHave(text(TITLE_TEXT));
 
     }
 
     public void checkResult(String key, String value) {
-        $(".table-responsive table").$(byText(key)).parent().shouldHave(text(value));
+        resultsTable.$(byText(key)).parent().shouldHave(text(value));
 
     }
 
     public void checkCloseModal() {
-        $("#closeLargeModal").click();
-        $(".modal-dialog").should(disappear);
+        closeModalWindowButton.click();
+        modalWindow.should(disappear);
     }
 }
 
