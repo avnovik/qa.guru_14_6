12dcecdaf3854ca136850aaf05270e652e1eb0c3
Novikov Aleksei <3900676@gmail.com>
2022-08-22 22:22:16 +0300

    added ResultsTableComponent +  Pictures

---
+++ src/test/java/com/demoqa/pages/components/ResultsTableComponent.java
@@ -0,0 +1,32 @@
+package com.demoqa.pages.components;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class ResultsTableComponent {
+
+    private final static String TITLE_TEXT = "Thanks for submitting the form";
+
+    public ResultsTableComponent checkVisible() {
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text(TITLE_TEXT));
+
+        return this;
+    }
+
+    public ResultsTableComponent checkResult(String key, String value) {
+        $(".table-responsive table").$(byText(key)).parent().shouldHave(text(value));
+
+        return this;
+    }
+
+    public void checkCloseModal() {
+        $("#closeLargeModal").click();
+        $(".modal-dialog").should(disappear);
+    }
+}
+
+
+
